8cd4bbd337467c7afd47b16c7c33afe24b5f06d8
yhori991 <>
2019-04-18 22:13:49 +0900

    Added stream

--- s3file/__init__.py
+++ s3file/__init__.py
@@ -4,3 +4,4 @@ from .__S3File import s3_upload as upload
 from .__S3File import s3_load as load
 from .__S3File import s3_save as save
 from .__S3File import s3_open as open
+from .__S3File import s3_stream as stream
