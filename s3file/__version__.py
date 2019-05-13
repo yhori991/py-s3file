f62c5778ece0a07cd31eeeae1dad21c30d98d698
yhori991 <>
2019-05-13 17:21:58 +0900

    Bumped up to 0.0.5

--- s3file/__version__.py
+++ s3file/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'py-s3file'
 __description__ = 'A simple utility tool to access s3 object through basic File I/O operations.'
 __copywrite__ = 'Copyright (C) yhori991'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __license__ = 'MIT'
 __author__ = 'yhori991'
 __author_email__ = 'yhori991@gmail.com'
